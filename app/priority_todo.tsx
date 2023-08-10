699559a3c46270bc8a0ec309c8bdc72d995199fb
alishia-s <alishia.seo@utah.edu>
2023-08-10 00:08:20 -0600

    fixed some issues I think?

--- app/priority_todo.tsx
+++ app/priority_todo.tsx
@@ -1,5 +1,5 @@
 import { } from "tamagui"
-<<<<<<< HEAD
+
 import React, { useState, createElement } from 'react'
 import { View, ScrollView, FlatList, Text, TextInput, Image } from 'react-native'
 import BouncyCheckbox from "react-native-bouncy-checkbox";
@@ -7,15 +7,6 @@ import { } from "expo-router"
 import priority_ui_styles from "./priority_todo_list_stylesheet"
 import {Images} from '../assets/images'
 import { TouchableOpacity } from "react-native-gesture-handler"
-=======
-import React from 'react'
-import { View, ScrollView, FlatList, Text, TextInput } from 'react-native'
-import BouncyCheckbox from "react-native-bouncy-checkbox";
-import { } from "expo-router"
-import priority_ui_styles from "./priority_todo_list_stylesheet"
-// import { Images } from '../assets/images';
-import { TouchableOpacity } from "react-native-gesture-handler";
->>>>>>> e71ebcd4b2da692d64106d4475ce58be33ba8ea4
 
 //Variables
 var date = new Date();
@@ -99,7 +90,7 @@ function PriorityUI() {
                     }}>
                 </FlatList>
                 <View style={priority_ui_styles.input_bar}>
-                    <Image source = {Images.home_bar}></Image>
+                    {/* <Image source = {Images.home_bar}></Image> */}
                 </View>
             </View>
         </View>
